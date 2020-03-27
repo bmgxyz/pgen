f71a822bec8d02a2e9fbddac639669a0446584e7
Bradley Gannon <bradley@bradleygannon.com>
2020-03-27 17:41:04 -0400

    Add LENGTH parameter
    Added command line argument parsing with `clap`. First and only argument
    so far is `LENGTH`, which optionally specifies the number of characters
    in the random password. Defaults to 16.
    
    Also added some TODOs and changed name to `pgen`.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,59 @@
 use rand::{thread_rng, Rng};
+use clap::{App};
 
 fn get_random_character() -> char {
     // choose a random character between `a` and `z`
+    // TODO make character set configurable (both w/ preset and regex)
     let mut rng = thread_rng();
     let random_char : u8 = rng.gen_range(97, 122+1);
     random_char as char
 }
 
 fn main() {
+    // set up command line arguments
+    let mut app = App::new("pgen")
+                    .version("0.2.0")
+                    .author("Bradley Gannon <bradley@bradleygannon.com>")
+                    .about("Generates random passwords")
+                    .args_from_usage("
+                        [LENGTH] number of characters in the password");
+    let matches = app.clone().get_matches();
+
     // set password length
-    // TODO command line argument
-    let password_len : u8 = 16;
+    const DEFAULT_PASSWORD_LENGTH : u8 = 16;
+    // if LENGTH is not a positive integer, then show help and quit
+    let password_len = match matches.value_of("LENGTH") {
+        Some(length_str) => {
+            match length_str.parse::<u8>() {
+                Ok(length_int) => {
+                    if length_int == 0 {
+                        app.print_help()
+                            .expect("ERROR: unable to print help");
+                        println!("\n\nERROR: LENGTH must be a positive integer");
+                        std::process::exit(1);
+                    }
+                    length_int
+                },
+                Err(_error) => {
+                    app.print_help()
+                        .expect("ERROR: unable to print help");
+                    println!("LENGTH must be a positive integer");
+                    std::process::exit(1);
+                }
+            }
+        },
+        None => {
+            // if the user didn't specify a LENGTH, use the default
+            DEFAULT_PASSWORD_LENGTH
+        }
+    };
 
     // keep track of how many characters we've got
     let mut num_chars : u8 = 0;
     while num_chars < password_len {
         // keep printing characters until we have enough
+        // TODO give the user the option to have the output hidden on screen
+        // (but still copyable)
         print!("{}", get_random_character());
         num_chars += 1;
     }
